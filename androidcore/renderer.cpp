47990043f08707a8de4a3992ba6a3ab729636125
Phyninite <jakajakamd@gmail.com>
2025-05-30 15:41:54 -0500

    Fixed renderer.cpp
    I will change it to C instead.

--- androidcore/renderer.cpp
+++ androidcore/renderer.cpp
@@ -15,33 +15,7 @@ private:
     EGLDisplay display;
     EGLSurface egl_surface;
     EGLContext context;
-    GLuint shader_program;
-    GLuint vao, vbo;
     int screen_width, screen_height;
-
-    const char* vertex_shader_source = R"(
-#version 320 es
-layout (location = 0) in vec2 a_pos;
-void main() {
-    gl_Position = vec4(a_pos, 0.0, 1.0);
-}
-)";
-
-    const char* fragment_shader_source = R"(
-#version 320 es
-precision mediump float;
-out vec4 frag_color;
-void main() {
-    frag_color = vec4(0.0, 0.6, 0.2, 0.8);
-}
-)";
-
-    const float vertices[8] = {
-        -0.3f, -0.2f,
-         0.3f, -0.2f,
-         0.3f,  0.2f,
-        -0.3f,  0.2f
-    };
 
 public:
     bool initialize() {
@@ -97,12 +71,7 @@ public:
         context = eglCreateContext(display, config, EGL_NO_CONTEXT, context_attribs);
         eglMakeCurrent(display, egl_surface, egl_surface, context);
 
-        setup_shaders();
-        setup_geometry();
-
         glViewport(0, 0, screen_width, screen_height);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
         return true;
     }
@@ -110,19 +79,10 @@ public:
     void render() {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        glUseProgram(shader_program);
-        glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-        
         eglSwapBuffers(display, egl_surface);
     }
 
     void cleanup() {
-        glDeleteVertexArrays(1, &vao);
-        glDeleteBuffers(1, &vbo);
-        glDeleteProgram(shader_program);
-        
         eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
         eglDestroyContext(display, context);
         eglDestroySurface(display, egl_surface);
@@ -130,38 +90,5 @@ public:
         
         surface_control.clear();
         client.clear();
-    }
-
-private:
-    void setup_shaders() {
-        GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
-        glCompileShader(vertex_shader);
-
-        GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
-        glCompileShader(fragment_shader);
-
-        shader_program = glCreateProgram();
-        glAttachShader(shader_program, vertex_shader);
-        glAttachShader(shader_program, fragment_shader);
-        glLinkProgram(shader_program);
-
-        glDeleteShader(vertex_shader);
-        glDeleteShader(fragment_shader);
-    }
-
-    void setup_geometry() {
-        glGenVertexArrays(1, &vao);
-        glGenBuffers(1, &vbo);
-
-        glBindVertexArray(vao);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-
-        glBindVertexArray(0);
     }
 };
