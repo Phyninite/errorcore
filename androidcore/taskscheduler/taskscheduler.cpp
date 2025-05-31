0e1f996aacd5271b6248f76c63740ef0463496bd
Phyninite <jakajakamd@gmail.com>
2025-05-31 10:41:50 -0500

    Update taskscheduler.cpp

--- androidcore/taskscheduler/taskscheduler.cpp
+++ androidcore/taskscheduler/taskscheduler.cpp
@@ -1,34 +1,37 @@
 #include "pch.h"
 #include "logs.h"
 #include "Update.hpp"
-#include "thirdparty-includes/Hook/And64InlineHook.hpp"
+#include "../memory/memory.hpp"
+#include "../thirdparty-includes/Hook/And64InlineHook.hpp"
 
-#include "pch.h"
-#include "logs.h"
-#include "Update.hpp"
-#include "thirdparty-includes/Hook/And64InlineHook.hpp"
+#ifdef __aarch64__
+#error "This code is for 32-bit ARM only"
+#endif
 
 #define whsj "WaitingHybridScriptsJob"
 
 void* original_job_start = nullptr;
-int64_t job_list[10];
+uintptr_t job_list[10];
 int job_list_index = 0;
 
-int64_t hooked_job_start(int64_t a1) {
+uintptr_t hooked_job_start(uintptr_t a1) {
     auto retrieved = a1;
     
     job_list[job_list_index % 10] = retrieved;
     job_list_index++;
     
-    if (retrieved == (int64_t)whsj) {
+    if (strcmp((char*)retrieved, whsj) == 0) {
         auto rLenc = retrieved + 512;
-        LOGD("whsj: 0x%llx", retrieved);
+        LOGD("whsj: 0x%x", retrieved);
     }
-    LOGD("WaitingHybridScriptsJob: 0x%llx", retrieved);
-    ((int64_t(*)(int64_t))original_job_start)(a1);
+    LOGD("WaitingHybridScriptsJob: 0x%x", retrieved);
+    ((uintptr_t(*)(uintptr_t))original_job_start)(a1);
     return retrieved;
 }
 
 void init_task_scheduler() {
-    A64HookFunction(Addresses::job_start, (void*)hooked_job_start, &original_job_start);
+#ifdef __aarch64__
+    *((int*)0) = 0;
+#endif
+    A64HookFunction((void*)memory::rebaseAddress(Addresses::job_start), (void*)hooked_job_start, &original_job_start);
 }
